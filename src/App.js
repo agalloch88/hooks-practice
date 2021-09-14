65f75c31d4c0e4a475a74adaddfa6e9d365a3da7
Ryan Kirsch <ryan.kirsch@longevityconsulting.com>
2021-09-14 13:25:11 -0400

    implement useContext to render tree data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React, { useContext } from 'react';
 import './App.css';
+import { TreesContext } from './';
 
 function App() {
+
+  const { trees } = useContext(TreesContext);
 
   return (
     <div>
       <h1>Trees I've Heard Of</h1>
+      <ul>
+        {trees.map(tree => (<li key={tree.id}>{tree.type}</li>))}
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
