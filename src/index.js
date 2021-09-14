c3a224198904de0a92441f4994346cb24096a559
Ryan Kirsch <ryan.kirsch@longevityconsulting.com>
2021-09-14 07:29:23 -0400

    star colorizer example employing useState

--- src/index.js
+++ src/index.js
@@ -2,23 +2,43 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import { FaStar } from "react-icons/fa";
 
-function App() {
+// creates array of stars, value of which can be manipulated and changed
 
-  const [checked, setChecked] = useState(false);
+const createArray = (length) => [...Array(length)];
+
+// function for each individual star. starts as gray, when chlicked onSelect fires and changes color to red
+
+function Star({ selected = false, onSelect }) {
+  return (
+    <FaStar 
+      color={selected ? "red" : "gray"}
+      onClick={onSelect}  
+    />
+  );
+}
+
+function StarRating({ totalStars = 5 }) {
+  // useState manages the selection and counting of stars and dynamically updates
+  const [selectedStars, setSelectedStars] = useState(0);
 
   return (
-    <div>
-      <input 
-        type="checkbox" 
-        value={checked} 
-        onChange={() => 
-          setChecked((checked) => !checked)
-        }
-      />
-      <p>{checked ? "Checked!" : "Not Checked!"}</p>
-    </div>
-  )
+    <>
+      {createArray(totalStars).map((n, i) => 
+        (<Star 
+          key={i} 
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>{selectedStars} of {totalStars}</p>
+    </>
+  );
+}
+
+function App() {
+  return <StarRating totalStars={10} />;
 }
 
 ReactDOM.render(
