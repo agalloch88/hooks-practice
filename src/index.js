32a49ea53aa5146f773c6fc807e19a14a7b50dca
Ryan Kirsch <ryan.kirsch@longevityconsulting.com>
2021-09-14 06:50:31 -0400

    useState example with 'deliver' button and status message

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+function App() {
+  const [status, setStatus] = useState("Not Delivered");
+
+  return (
+    <div>
+      <h1>The package is: {status}</h1>
+      <button onClick={() => setStatus("Delivered")}>Deliver</button>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
